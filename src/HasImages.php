9cf102b44347d0015973e3e373588800d501f0f5
zingimmick <zingimmick@outlook.com>
2021-03-17 12:16:09 +0800

    Add priority

--- src/HasImages.php
+++ src/HasImages.php
@@ -133,8 +133,8 @@ trait HasImages
                 return [
                     $image->getKey() => [
                         'priority' => $key,
-                        
-                    ], ];
+                    ],
+                ];
             }
         )->toArray());
 
